6027e2ef46de3f04cd698982013372edb2c1e9aa
crrmacarse <crrmacarse@gmail.com>
2024-03-13 07:32:48 +0800

    feat: initial routes

---
+++ src/constants/route.ts
@@ -0,0 +1,10 @@
+export const HOME_PAGE = '/';
+export const LOGIN_PAGE = '/login';
+export const SIGNUP_PAGE = '/signup';
+export const FORGOT_PASSWORD_PAGE = '/forgot-password';
+
+export const ADMIN_PAGE = '/admin';
+export const ADMIN_PROFILE_PAGE = '/admin/profile'
+
+export const MASTERADMIN_PAGE = '/masteradmin';
+export const MASTERADMIN_SIGNUP_PAGE = '/masteradmin/signups';

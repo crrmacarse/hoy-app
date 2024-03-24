3267c62aa3f690f7a25796e952e60d4720f123aa
crrmacarse <crrmacarse@gmail.com>
2024-03-24 23:37:24 +0800

    feat: added error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const emojis = {
+		// TODO add the rest!
+		420: '🫠',
+		500: '💥'
+	};
+</script>
+
+<h1>{$page.status} {$page.error?.message}</h1>
+
+<p>
+	{emojis[$page.status] ?? emojis[500]}
+</p>
